cb1299789c99de3984df3cc59a25789a1c518b3e
Olek <roguezilla0@gmail.com>
2020-09-15 13:30:20 +0100

    file system overhaul, token bug fix

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
-from flask import *
+from flask import (
+    Flask,
+    request,
+    g,
+    render_template,
+    session,
+    redirect,
+    url_for,
+    send_file
+)
 import dataset
 from io import BytesIO
 import random
 import string
+import os
+import humanize
 
 from salt import hash
 
 app = Flask('Neverland')
+app.config['UPLOAD_FOLDER'] = 'files'
 app.secret_key = 'mwz!R&n$%Fh*TBHkk@Ksare=vjkRQ4Xmz%fCm=yFGsDfyZA^T97%JWXtp^3#C+Y*g&eJdx9crb4^7_BWNxt4sZYJ_4NFmLJ2%s3U+@%*_-py5z3VQ?$m^X8y?j?jaN^*'
-file_db = dataset.connect('sqlite:///file_database.db')
+
 user_db = dataset.connect('sqlite:///user_database.db')
+file_db = dataset.connect('sqlite:///file_database.db')
 
 @app.before_request
 def before_request():
     g.user = None
 
     if 'token' in session:
-        if user_db['users'].find_one(token=session['token'])['ip'] == request.remote_addr:
-            g.user = user_db['users'].find_one(token=session['token'])['name']
-        else:
-            user_db['users'].update(dict(name=user_db['users'].find_one(token=session['token'])['name'], token=''), ['name'])
+        if user_db['users'].find_one(token=session['token']) is None:
             session.pop('token', None)
+        else:
+            if user_db['users'].find_one(token=session['token'])['ip'] == request.remote_addr:
+                g.user = user_db['users'].find_one(token=session['token'])['name']
+            else:
+                user_db['users'].update(dict(name=user_db['users'].find_one(token=session['token'])['name'], token=''), ['name'])
+                session.pop('token', None)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -48,7 +64,7 @@ def login():
 def index():
     if not g.user:
         return redirect(url_for('login'))
-    
+
     return render_template('index.html', files=file_db['files'])
 
 @app.route('/upload', methods=['POST'])
@@ -56,15 +72,22 @@ def upload():
     if not g.user:
         return redirect(url_for('login'))
     
-    file = request.files['inputFile']
-    if file.filename != '' and file_db['files'].find_one(name=str(file.filename)) is None:
-        file_db['files'].insert(dict(name=file.filename, data=file.read()))
+    files = request.files.getlist('inputFile')
+    for file in files:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath) 
+
+        file_db['files'].insert(dict(
+            name=file.filename,
+            path=filepath,
+            size=humanize.naturalsize(os.stat(filepath).st_size)
+        ))
 
     return redirect(url_for('index'))
 
 @app.route('/download/<filename>')
-def download(filename):
+def download(filename: str):
     if not g.user:
         return redirect(url_for('login'))
     
-    return send_file( BytesIO(file_db['files'].find_one(name=str(filename))['data']), attachment_filename=str(filename), as_attachment=True)
+    return send_file(file_db['files'].find_one(name=filename)['path'], attachment_filename=filename, as_attachment=True)
